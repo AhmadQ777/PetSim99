e11e0f21e5afb8a6825703b2a2ecb60658d5fc72
AhmadQ777 <ahmadsheikosman2011@gmail.com>
2026-06-19 15:44:48 +0200

    s

--- Main/Test.lua
+++ Main/Test.lua
@@ -1,4 +1,3 @@
 local TPS = game:GetService("TeleportService")
 local Players = game:GetService("Players")
-local Player = Players.LocalPlayer
-TPS:Teleport(8737899170, Player)
+TPS:Teleport(8737899170, Players.LocalPlayer)
