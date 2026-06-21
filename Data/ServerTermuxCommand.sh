ac8cd37b7372e07bbd25c1c4df62993f9e19abf8
AhmadQ777 <ahmadsheikosman2011@gmail.com>
2026-06-21 18:10:03 +0200

    Chnaged Folder Name from Server to Data

--- Data/ServerTermuxCommand.sh
+++ Data/ServerTermuxCommand.sh
@@ -1,13 +1,21 @@
 pkg update -y && pkg upgrade -y
-pkg install python curl -y
+
+pkg install python git curl -y
 
 pip install requests -q
+
+# Storage Zugriff aktivieren
+termux-setup-storage
 
 termux-wake-lock
 
 mkdir -p ~/ps99
 cd ~/ps99
 
+# initial download
+curl -s "https://raw.githubusercontent.com/AhmadQ777/PetSim99/main/Server/builder.py" -o builder.py
+
+# main loop
 while true; do
     curl -s "https://raw.githubusercontent.com/AhmadQ777/PetSim99/main/Server/builder.py" -o builder.py
     python builder.py
