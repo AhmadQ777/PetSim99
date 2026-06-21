3a5c6da4fc2e5122986bd7ab77dec8f785c67aef
AhmadQ777 <ahmadsheikosman2011@gmail.com>
2026-06-21 18:49:38 +0200

    Shortend ServerTermuxCommand.sh

--- Data/ServerTermuxCommand.sh
+++ Data/ServerTermuxCommand.sh
@@ -1,18 +1,10 @@
 pkg update -y && pkg upgrade -y
-
 pkg install python git curl -y
-
 pip install requests -q
-
-# Storage Zugriff aktivieren
 termux-setup-storage
-
 termux-wake-lock
-
 mkdir -p ~/ps99
 cd ~/ps99
-
-# main loop
 while true; do
     curl -s "https://raw.githubusercontent.com/AhmadQ777/PetSim99/main/Data/builder.py" -o builder.py
     python builder.py
