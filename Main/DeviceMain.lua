20fa2a819d4d5ede78f930da0599fb61872b8a89
AhmadQ777 <ahmadsheikosman2011@gmail.com>
2026-06-21 12:13:11 +0200

    a

--- Main/DeviceMain.lua
+++ Main/DeviceMain.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadQ777/PetSim99/refs/heads/main/Main/Main.lua"))()()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadQ777/PetSim99/refs/heads/Fixing_bugs/Main/Main.lua"))()()
