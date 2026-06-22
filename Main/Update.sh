2365210ee0826437087a814a6bad122cc1928b5c
AhmadQ777 <ahmadsheikosman2011@gmail.com>
2026-06-22 16:23:25 +0200

    Files: builder.py, Update.sh -m Main.Folder

--- Main/Update.sh
+++ Main/Update.sh
@@ -1,10 +1,14 @@
 pkg update -y && pkg upgrade -y
+
 pkg install python git curl -y
 pip install requests -q
+
 termux-setup-storage
 termux-wake-lock
+
 mkdir -p ~/ps99
 cd ~/ps99
+
 while true; do
     curl -s "https://raw.githubusercontent.com/AhmadQ777/PetSim99/main/Data/builder.py" -o builder.py
     python builder.py
