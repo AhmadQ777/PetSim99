f55cc6b47a21c9796f86df8d6fbad51f4f28f613
AhmadQ777 <ahmadsheikosman2011@gmail.com>
2026-07-02 20:08:48 +0200

    d

---
+++ Test copy.lua
@@ -0,0 +1,2 @@
+local button = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Interact"):WaitForChild("Button")
+firesignal(button.Activated)
--- Test.lua
+++ Test.lua
@@ -5,6 +5,18 @@ local events = {
     MouseButton1Click = button.MouseButton1Click,
     MouseButton1Down = button.MouseButton1Down,
     MouseButton1Up = button.MouseButton1Up,
+    MouseButton2Click = button.MouseButton2Click,
+    MouseButton2Down = button.MouseButton2Down,
+    MouseButton2Up = button.MouseButton2Up,
+    MouseEnter = button.MouseEnter,
+    MouseLeave = button.MouseLeave,
+    MouseMoved = button.MouseMoved,
+    SelectionGained = button.SelectionGained,
+    SelectionLost = button.SelectionLost,
+    TouchLongPress = button.TouchLongPress,
+    InputBegan = button.InputBegan,
+    InputChanged = button.InputChanged,
+    InputEnded = button.InputEnded,
 }
 for eventName, signal in pairs(events) do
     local ok, connections = pcall(getconnections, signal)
