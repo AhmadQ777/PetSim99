3ae8db32d1a4bfce2bd689263f2b026f26dfddfd
AhmadQ777 <ahmadsheikosman2011@gmail.com>
2026-07-02 20:32:10 +0200

    d

--- Test2.lua
+++ Test2.lua
@@ -1,5 +1,11 @@
-firesignal(game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Interact"):WaitForChild("Button").InputBegan)
+local button = game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Interact"):WaitForChild("Button")
+local input = {
+    UserInputType = Enum.UserInputType.MouseButton1,
+    UserInputState = Enum.UserInputState.Begin,
+}
+firesignal(button.InputBegan, input)
 print("1")
-task.wait(10)
-firesignal(game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Interact"):WaitForChild("Button").InputEnded)
+task.wait(15)
+input.UserInputState = Enum.UserInputState.End
+firesignal(button.InputEnded, input)
 print("2")
